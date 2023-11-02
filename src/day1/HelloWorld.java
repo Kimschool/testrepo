14824f41ffb4ec7393e00cdb2a77eebc280dfd9a
WEAVUS-SURFACE\clils <clilseoo2@gmail.com>
2023-11-02 17:04:25 +0900

    init commit

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,40 @@
+package day1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		int pak = 1;
+
+		if(1 == 1) {
+			int an = 1;
+			System.out.println(pak);
+		}
+
+		System.out.println(pak);
+
+
+		System.out.println(1234);
+		System.out.print(1234);
+		System.out.println("Hello World");
+
+		System.out.println(1+1+"1"+1);
+
+		long num1 = 2200000000L;
+		float num2 = 1.5F;
+
+		String str1 = "나는 \" 자바\" 를좋아합니다.";
+		double var = 100.0;
+
+
+		// TODO : 나카무상기능 코딩완료후 결합 예정
+
+		// 아래의 메소드....
+
+
+
+	}
+
+
+
+}
